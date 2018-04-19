7c55f94fae304c41599f904ab7434b42bae8951a
Programmer Muslim <rizki@imastudio.co.id>
2018-04-19 08:40:05 +0700

    add

--- MapKitFlyOverIOSSwfit/MapKitFlyOverIOSSwfit/ViewController.swift
+++ MapKitFlyOverIOSSwfit/MapKitFlyOverIOSSwfit/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
+    
+    let distance: CLLocationDistance = 800
+    let pitch: CGFloat = 400
+    let heading = 45.0
+    var camera: MKMapCamera?
 
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
@@ -17,6 +22,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btnRotateRight(_ sender: Any) {
+    }
+    @IBAction func btnLeft(_ sender: Any) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
